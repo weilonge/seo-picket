02b6c5abedaa769b4ccc6f8ae3e26f28b6588b59
Sean Lee <weilonge@gmail.com>
2018-03-22 18:46:37 +0800

    Implement test cases and eslint check.

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var gulp = require('gulp');
+var gutil = require('gutil');
+var mocha = require('gulp-mocha');
+var eslint = require('gulp-eslint');
+
+const TEST_PATH = 'test/*_test.js';
+
+gulp.task('unit-test', () => {
+  return gulp.src([TEST_PATH], {read: false})
+    .pipe(mocha({reporter: 'list'})).on('error', gutil.log);
+});
+
+gulp.task('test', ['unit-test']);
+
+gulp.task('default', ['test']);
+
+gulp.task('lint', () => {
+  return gulp.src(['**/*.js','!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
---
+++ test/SEOPicket.js
@@ -0,0 +1,51 @@
+const Picket = require('../src/seo-picket');
+const assert = require('chai').assert;
+const { Writable } = require('stream');
+
+class OutputStream extends Writable {
+  constructor() {
+    super();
+    this._result = '';
+  }
+
+  _write(chunk, encodiing, callback) {
+    this._result += chunk;
+    callback();
+  }
+
+  get result() {
+    return this._result;
+  }
+}
+
+describe('Validate all rules', function() {
+  it('<head></head> should contain <title> and the related <meta>.', async function () {
+    const stream = new OutputStream();
+    let picket = new Picket({
+      rules: {
+        h1: {},
+        head: {},
+        img: {},
+        a: {},
+        strong: {
+          tagCount: 15,
+        },
+        customRules: {
+          run: function($, output) {
+            output('my custom rule');
+          },
+        },
+      },
+      output: 'stream',
+      outputOpt: stream,
+    });
+    await picket.checkString('<head></head>');
+    assert.equal(
+      stream.result,
+      'No <title> tag in <head>\n' +
+      'No <meta name="description"> tag in <head>\n' +
+      'No <meta name="keywords"> tag in <head>\n' +
+      'my custom rule\n'
+    );
+  });
+});
