105aa8a40c65e874864a70c2c63cb123dcdc35de
Sean Lee <weilonge@gmail.com>
2019-06-11 20:59:17 +0800

    Upgrade gulp to 4.

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,19 @@ var eslint = require('gulp-eslint');
 
 const TEST_PATH = 'test/*_test.js';
 
-gulp.task('unit-test', () => {
+gulp.task('unit-test', gulp.series(() => {
   return gulp.src([TEST_PATH], {read: false})
     .pipe(mocha({reporter: 'list'})).on('error', gutil.log);
-});
+}));
 
-gulp.task('test', ['unit-test']);
+gulp.task('test', gulp.series('unit-test'));
 
-gulp.task('default', ['test']);
+gulp.task('default', gulp.series('test'));
 
-gulp.task('lint', () => {
+gulp.task('lint', gulp.series(() => {
   return gulp.src(['**/*.js','!node_modules/**'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
-});
+}));
 
